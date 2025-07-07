eb4c0e1b0d65e457f4b28d4c23784b80f562d8d7
Verper1 <slavabuk8@gmail.com>
2025-07-07 15:45:37 +0300

    Добавил тесты и узнал как можно сделать тест на вызываемую ошибку. Также, исправил логику создания родительского класса, заменив просто print на raise.

--- polimorphism.py
+++ polimorphism.py
@@ -1,23 +1,37 @@
 # Polymorphism with a Function: Create a function describe_pet that takes an object of Animal and calls
 # its speak method, demonstrating polymorphism.
+import pytest
 
 
 class Animal:
     def speak(self):
-        print('Метод speak Animal')
+        print("NotImplementedError(\"Subclasses must implement this method\")")
+        raise NotImplementedError("Subclasses must implement this method")
 
 
 class Cat(Animal):
     def speak(self):
         print('Метод speak Cat')
+        return 'Метод speak Cat'
+
+
+class Dog(Animal):
+    def speak(self):
+        print('Метод speak Dog')
+        return 'Метод speak Dog'
 
 
 def speak(animal):
-    animal.speak()
+    return animal.speak()
 
 
-a1 = Animal()
-c1 = Cat()
+def test_func():
+    c1 = Cat()
+    d1 = Dog()
+    a1 = Animal()
 
-speak(a1)
-speak(c1)
+    assert speak(c1) == "Метод speak Cat"
+    assert speak(d1) == "Метод speak Dog"
+    with pytest.raises(NotImplementedError) as exc_info:  # Чтобы тест проверял, что вызов метода speak(a1) действительно вызывает исключение NotImplementedError с указанным сообщением, нужно использовать pytest.raises в контекстном менеджере
+        a1.speak()
+    assert str(exc_info.value) == "Subclasses must implement this method"
