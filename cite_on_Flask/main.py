82b0f30ee5bf42b017b5499e6974e70b8aebd066
Verper1 <slavabuk8@gmail.com>
2025-07-08 17:37:44 +0300

    Сделал страницу входа в админ панель и саму админ панель, но пока это всё не работает должным образом.

--- cite_on_Flask/main.py
+++ cite_on_Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ class Notes(db.Model):
 
     def __repr__(self):
         return self.title
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -34,9 +35,27 @@ def index():
     else:
         return render_template('index.html', data=notes)
 
+
+@app.route('/login_adm_page', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        # Здесь должна быть логика аутентификации
+        # Если аутентификация прошла успешно, перенаправляем на /admin_panel
+        return redirect(url_for('admin_panel'))
+    # Если GET запрос, показываем форму входа
+    return render_template('login_adm_page.html')
+
+# страница админ панели
+@app.route('/admin_panel')
+def admin_panel():
+    # Загрузка и отображение админ-панели
+    return render_template('admin_panel.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/create', methods=['POST', 'GET'])
 def crate():
@@ -54,6 +73,7 @@ def crate():
             return f'Ошибка: {e}'
     else:
         return render_template('create.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
