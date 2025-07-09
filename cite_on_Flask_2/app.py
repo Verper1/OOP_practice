38aab572112267f969ffec44e4e21a4624eeaaba
Verper1 <slavabuk8@gmail.com>
2025-07-09 23:09:26 +0300

    Сделал страницы-заглушки для сайта. Реализацию захода в аккаунт и регистрации сделаю позже, также как и с комментариями.

--- cite_on_Flask_2/app.py
+++ cite_on_Flask_2/app.py
@@ -7,5 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/comments')
+def comments():
+    return render_template('comments.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
